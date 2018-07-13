5780b1b5c4c6ec1ab9d1ce9b765a189eb44da6dd
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-13 11:35:30 +0100

    Added the pretty add function

--- lib/calc_pretty.rb
+++ lib/calc_pretty.rb
@@ -1,8 +1,5 @@
-class Calculator
+class CalculatorPretty
     def add (x,y)
-        x + y
-    end
-    def pretty_add (x,y)
         "The sum of #{x} and #{y} is #{x +y}"
     end
 end
--- spec/calc_pretty_spec.rb
+++ spec/calc_pretty_spec.rb
@@ -1,25 +1,19 @@
-require 'calculator'
-describe Calculator do
+require 'calc_pretty'
+describe CalculatorPretty do
     it 'responds to method add' do
        expect(subject). to respond_to (:add)
     end
     it 'responds to method add with two arguments' do
         expect(subject). to respond_to(:add).with(2).argument
     end
-    it 'gives 2 when passed 1 and 1 as arguments' do
-        expect(subject.add(1,1)).to eq 2
-    end
-    it 'gives 3 when passed 1 and 2 as arguments' do
-        expect(subject.add(1,2)).to eq 3
-    end
 
     # So I can see pretty results
     # I want to add two numbers and get "the sum of a and b is result"
 
     it 'gives "The sum of 1 and 1 is 2" when passed 1 and 1 as arguments' do
-        expect(subject.pretty_add(1,1)).to eq "The sum of 1 and 1 is 2"
+        expect(subject.add(1,1)).to eq "The sum of 1 and 1 is 2"
     end
     it 'gives "The sum of 1 and 2 is 3" when passed 1 and 2 as arguments' do
-        expect(subject.pretty_add(1,2)).to eq "The sum of 1 and 2 is 3"
+        expect(subject.add(1,2)).to eq "The sum of 1 and 2 is 3"
     end
 end
